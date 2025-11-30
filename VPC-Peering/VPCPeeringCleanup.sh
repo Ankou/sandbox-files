8c525a54541553be0f19ac3299cf51535a428ef4
Ankou <rickyjfield@hotmail.com>
2025-11-30 21:53:52 +1100

    Updated VPCPeeringCleanup.sh

--- VPC-Peering/VPCPeeringCleanup.sh
+++ VPC-Peering/VPCPeeringCleanup.sh
@@ -29,6 +29,7 @@ igwB=$( jq -r .igwB $resources )
 igwC=$( jq -r .igwC $resources )
 
 # Delete Subnets
+echo -e "\e[31mDeleting Subnets\e[0m"
 aws ec2 delete-subnet --subnet-id $subnetAPub
 aws ec2 delete-subnet --subnet-id $subnetAPriv
 aws ec2 delete-subnet --subnet-id $subnetBPub
@@ -37,21 +38,25 @@ aws ec2 delete-subnet --subnet-id $subnetCPub
 aws ec2 delete-subnet --subnet-id $subnetCPriv
 
 # Delete Route tables
+echo -e "\e[31mDeleting Route Tables\e[0m"
 aws ec2 delete-route-table --route-table-id $privRTA
 aws ec2 delete-route-table --route-table-id $privRTB
 aws ec2 delete-route-table --route-table-id $privRTC
 
 # Detach Internet Gateway
+echo -e "\e[31mDetatching Internet Gateways\e[0m"
 aws ec2 detach-internet-gateway --internet-gateway-id $igwA
 aws ec2 detach-internet-gateway --internet-gateway-id $igwB
 aws ec2 detach-internet-gateway --internet-gateway-id $igwC
 
 # Delete Internet Gateway
+echo -e "\e[31mDeleting Internet Gateways\e[0m"
 aws ec2 delete-internet-gateway --internet-gateway-id $igwA
 aws ec2 delete-internet-gateway --internet-gateway-id $igwB
 aws ec2 delete-internet-gateway --internet-gateway-id $igwC
 
 # Delete VPC
+echo -e "\e[31mDeleting Subnets\e[0m"
 aws ec2 delete-vpc --vpc-id $VPCA
 aws ec2 delete-vpc --vpc-id $VPCB
 aws ec2 delete-vpc --vpc-id $VPCC
